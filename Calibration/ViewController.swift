f9ac9136bfa6a9192c16cc5c3d22b76816b34f96
Jonah Witcig <jwitcig@gmail.com>
2017-12-17 18:41:38 -0600

    .

--- Calibration/ViewController.swift
+++ Calibration/ViewController.swift
@@ -28,6 +28,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        
+        
+        let g = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        g.firstMaterial?.diffuse.contents = UIColor.purple
+        let ball = SCNNode(geometry: g)
+        ball.position = SCNVector3(x: -1.48086596, y: -0.0808541551, z: -0.0840704888)
+        sceneView.scene.rootNode.addChildNode(ball)
+        
+        
+        
+        
+        
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -76,5 +90,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            
+            let location = touch.location(in: self.view)
+            
+            if location.x < self.view.frame.width / 2 {
+                print(sceneView.session.currentFrame!.camera.transform.position)
+                
+                let g = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+                g.firstMaterial?.diffuse.contents = UIColor.purple
+                let ball = SCNNode(geometry: g)
+                ball.position = sceneView.session.currentFrame!.camera.transform.position
+                sceneView.scene.rootNode.addChildNode(ball)
+                
+                
+            } else {
+                let config = ARWorldTrackingConfiguration()
+                sceneView.session.run(config, options: .resetTracking)
+                print("reset: \(sceneView.session.currentFrame!.camera.transform.position)")
+                
+
+                let g = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+                g.firstMaterial?.diffuse.contents = UIColor.purple
+                let ball = SCNNode(geometry: g)
+                ball.position = SCNVector3(x: -1.48086596, y: -0.0808541551, z: -0.0840704888)
+                sceneView.scene.rootNode.addChildNode(ball)
+            }
+        
+        }
+    }
+}
+
+extension matrix_float4x4 {
+    var position: SCNVector3 {
+        return SCNVector3(columns.3.x, columns.3.y, columns.3.z)
     }
 }
